321a2e4afc288909f6f3996c0fff37227dd870e5
Cosmin0707 <cosmin.nicol135@gmail.com>
2025-07-10 21:25:46 +0300

    -tdl

---
+++ Organizer/organizer-app/src/App.jsx
@@ -0,0 +1,14 @@
+export default function App(){
+  return(
+    <div className="pageLayout">
+      <div className="sideNav">
+        <a href="">Dashboard</a>
+        <a href="">To-Do List</a>
+      </div>
+      <div className="mainContent">
+        <h1 className="welcomeHeader">Welcome back, Cosmin</h1>
+        <div className="contentBlock">Content</div>
+      </div>
+    </div>
+  );
+}
